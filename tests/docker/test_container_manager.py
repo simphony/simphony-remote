64ccd90f3f71b14733d9340fc01794a0cf0dcf04
Kit Yan Choi <kchoi@enthought.com>
2016-06-23 16:26:05 +0100

    added test for filtering non-existing volumes in ContainerManager

--- tests/docker/test_container_manager.py
+++ tests/docker/test_container_manager.py
@@ -1,3 +1,5 @@
+import os
+
 from remoteappmanager.docker.container import Container
 from remoteappmanager.docker.container_manager import ContainerManager
 from tests import utils
@@ -82,3 +84,29 @@ class TestContainerManager(AsyncTestCase):
         # Stop should have been called and the container removed
         self.assertTrue(mock_client.stop.called)
         self.assertTrue(mock_client.remove_container.called)
+
+    @gen_test
+    def test_start_container_with_nonexisting_volume_source(self):
+        # These volume sources are invalid
+        volumes = {'~no_way_this_be_valid': {'bind': '/target_vol1',
+                                             'mode': 'ro'},
+                   '/no_way_this_be_valid': {'bind': '/target_vol2',
+                                             'mode': 'ro'}}
+
+        # This volume source is valid
+        good_path = os.path.abspath('.')
+        volumes[good_path] = {'bind': '/target_vol3',
+                              'mode': 'ro'}
+
+        yield self.manager.start_container("username", "imageid", volumes)
+
+        # Call args and keyword args that craete_container receives
+        args = self.manager.docker_client.client.create_container.call_args
+        actual_volume_targets = args[1]['volumes']
+
+        # Invalid volume paths should have been filtered away
+        self.assertNotIn('/target_vol1', actual_volume_targets)
+        self.assertNotIn('/target_vol2', actual_volume_targets)
+
+        # Home directory is valid, should stay
+        self.assertIn('/target_vol3', actual_volume_targets)
