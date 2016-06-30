979deed9779c9806e90780109c3663f4f04c70f5
Kit Yan Choi <kchoi@enthought.com>
2016-06-30 17:00:22 +0100

    added verify token to the base handler and get_current_user

--- remoteappmanager/handlers/base_handler.py
+++ remoteappmanager/handlers/base_handler.py
@@ -1,5 +1,9 @@
+import requests
+from urllib.parse import urljoin, quote
+
 from tornado import web
-from urllib.parse import urljoin
+from tornado.httpclient import HTTPError
+from notebook.utils import url_path_join
 
 from remoteappmanager.logging.logging_mixin import LoggingMixin
 
@@ -8,7 +12,14 @@ class BaseHandler(web.RequestHandler, LoggingMixin):
     """Base class for the request handler."""
 
     def get_current_user(self):
-        return self.application.user.name
+        user_cookie = self.get_cookie(self.settings['cookie_name'])
+        if user_cookie:
+            verified = self.verify_token(self.settings['cookie_name'],
+                                         user_cookie)
+            if verified:
+                return self.application.user.name
+
+        return None
 
     def render(self, template_name, **kwargs):
         """Reimplements render to pass well known information to the rendering
@@ -17,8 +28,32 @@ class BaseHandler(web.RequestHandler, LoggingMixin):
         args = dict(
             user=self.current_user,
             base_url=self.application.config.base_url,
-            logout_url=urljoin(self.application.config.hub_prefix, "logout")
-        )
+            logout_url=urljoin(self.application.config.hub_prefix, "logout"))
 
         args.update(kwargs)
         super(BaseHandler, self).render(template_name, **args)
+
+    def verify_token(self, cookie_name, encrypted_cookie):
+        """Return True if cookie is verified as valid.
+        Otherwise, raise an HTTPError
+        """
+        hub_api_url = self.settings['hub_api_url']
+        hub_api_key = self.settings['hub_api_key']
+
+        r = requests.get(url_path_join(hub_api_url,
+                                       "authorizations/cookie",
+                                       cookie_name,
+                                       quote(encrypted_cookie, safe='')),
+                         headers={'Authorization': 'token %s' % hub_api_key})
+
+        if r.status_code == 403:
+            self.log.error("Auth token may have expired: [%i] %s",
+                           r.status_code, r.reason)
+            raise HTTPError(500,
+                            "Permission failure checking authorization, "
+                            "please restart.")
+        elif r.status_code >= 400:
+            self.log.warn("Failed to check authorization: [%i] %s",
+                          r.status_code, r.reason)
+            raise HTTPError(500, "Failed to check authorization")
+        return True
