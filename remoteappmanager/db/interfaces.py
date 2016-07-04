4c2eabfc735f4b794ecac344317549ff8c0d9913
Kit Yan Choi <kchoi@enthought.com>
2016-07-04 12:59:50 +0100

    Added ABCDatabase and ABCTestDatabase for the database interface

---
+++ remoteappmanager/db/interfaces.py
@@ -0,0 +1,80 @@
+from abc import ABCMeta, abstractmethod
+
+from traitlets import HasTraits, Unicode, Bool
+
+
+class ApplicationConfig(HasTraits):
+
+    #: An ID for this configuration
+    mapping_id = Unicode()
+
+    #: Name of the docker image
+    image = Unicode()
+
+    #: Whether user's home directory is mounted
+    allow_home = Bool()
+
+    #: Source path (on the host machine) for the application specific data
+    #: volume
+    volume_source = Unicode()
+
+    #: Target path (on the container) for the application specific data
+    #: volume
+    volume_target = Unicode()
+
+    #: Read-write access mode for the application specific data volume
+    #: (For docker, it is either "rw": read-write or "ro": read-only
+    volume_mode = Unicode()
+
+    def __eq__(self, other):
+        if not isinstance(other, ApplicationConfig):
+            return False
+
+        for trait_name in self.trait_names():
+            if getattr(self, trait_name) != getattr(other, trait_name):
+                return False
+
+        return True
+
+    def __hash__(self):
+        return hash(tuple((trait_name, getattr(self, trait_name))
+                          for trait_name in sorted(self.trait_names())))
+
+    def __repr__(self):
+        return "<{cls}({attrs})>".format(
+            cls=self.__class__.__name__,
+            attrs=", ".join("{0}={1!r}".format(
+                trait_name, getattr(self, trait_name))
+                            for trait_name in self.trait_names()
+                        )
+        )
+
+
+class ABCDatabase(metaclass=ABCMeta):
+
+    @abstractmethod
+    def get_user_by_name(self, user_name):
+        """ Return a User for a given user_name, or None
+        if the user name is not found.
+
+        Parameters
+        ----------
+        user_name : str
+
+        Returns
+        -------
+        user : User
+        """
+
+    @abstractmethod
+    def get_apps_for_user(self, user_name):
+        """ Return an iterable of ApplicationConfig for a given user name
+
+        Parameters
+        ----------
+        user_name: str
+
+        Returns
+        -------
+        iterable : ApplicationConfig
+        """
---
+++ tests/db/abc_test_interfaces.py
@@ -0,0 +1,33 @@
+from abc import abstractmethod, ABCMeta
+from collections import Iterable
+
+
+class ABCTestDatabase(metaclass=ABCMeta):
+
+    def setUp(self, usernames, application_configs):
+        if not (usernames and application_configs):
+            raise ValueError("usernames and application_configs must not "
+                             "be empty")
+
+        for configs in application_configs:
+            if not isinstance(configs, Iterable):
+                raise TypeError("{!0} is not iterable.".format(configs))
+
+        self.user_config_mappings = dict(zip(usernames, application_configs))
+
+    @abstractmethod
+    def create_database(self):
+        """ Create a database that complies with ABCDatabase """
+
+    @abstractmethod
+    def test_get_user_by_name(self):
+        """ Test ABCDatabase.get_user_by_name """
+
+    def test_get_apps_for_user(self):
+        """ Test get_appgs_for_user returns an iterable of ApplicationConfig
+        """
+        database = self.create_database()
+
+        for username, expected_configs in self.user_config_mappings.items():
+            actual_configs = database.get_apps_for_user(username)
+            self.assertSetEqual(set(actual_configs), set(expected_configs))
