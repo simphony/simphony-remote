29c5ec3b1a571d90a98afd0e5d4720e8f90cd164
Kit Yan Choi <kchoi@enthought.com>
2016-07-07 11:38:27 +0100

    Added tests for loading badly formatted csv and good csv with different header ordering

--- tests/db/test_csv_db.py
+++ tests/db/test_csv_db.py
@@ -8,21 +8,52 @@ from tests.db.abc_test_interfaces import ABCTestDatabaseInterface
 from tests.temp_mixin import TempMixin
 
 
-def write_csv_file(file_name):
-    _HEADERS = ('user.name', 'application.image', 'policy.allow_home',
-                'policy.allow_view', 'policy.allow_common',
-                'policy.volume_source', 'policy.volume_target',
-                'policy.volume_mode')
+class GoodTable:
+    headers = ('user.name', 'application.image', 'policy.allow_home',
+               'policy.allow_view', 'policy.allow_common',
+               'policy.volume_source', 'policy.volume_target',
+               'policy.volume_mode')
 
     #    name,     image, home, view, common, source, target, mode
-    _RECORDS = [
+    records = [
         ('foo', 'image_1', '1', '1', '0', '', '', ''),
         ('foo', 'image_2', '1', '1', '1', '/src', '/target', 'ro'),
         ('bar', 'image_1', '0', '0', '0', '/src', '/target', 'ro')]
 
+
+class BadTableMissingHeaders:
+    # policy.volume_source and policy.volume_target are missing
+    headers = ('user.name', 'application.image', 'policy.allow_home',
+               'policy.allow_view', 'policy.allow_common',
+               'policy.volume_mode')
+
+    #    name,     image, home, view, common, mode
+    records = [
+        ('foo', 'image_1', '1', '1', '0', ''),
+        ('foo', 'image_2', '1', '1', '1', 'ro'),
+        ('bar', 'image_1', '0', '0', '0', 'ro')]
+
+
+class GoodTableWithDifferentHeaders:
+    # There is an extra column and the columns order are different
+    # but that's okay
+    headers = ('policy.allow_view', 'policy.allow_common',
+               'policy.volume_source', 'policy.volume_target',
+               'policy.volume_mode',
+               'user.name', 'application.image', 'policy.allow_home',
+               'extra_column')
+
+    #  view, common, source, target, mode, name, image, home, extra
+    records = [
+        ('1', '0', '', '', '', 'foo', 'image_1', '1', 'anything'),
+        ('1', '1', '/src', '/target', 'ro', 'foo', 'image_2', '1', 'extra'),
+        ('0', '0', '/src', '/target', 'ro', 'bar', 'image_1', '0', 'whatever')]
+
+
+def write_csv_file(file_name, headers, records):
     with open(file_name, 'w') as csv_file:
-        print(*_HEADERS, sep=',', file=csv_file)
-        for record in _RECORDS:
+        print(*headers, sep=',', file=csv_file)
+        for record in records:
             print(*record, sep=',', file=csv_file)
 
 
@@ -34,7 +65,7 @@ class TestCSVAccounting(TempMixin, ABCTestDatabaseInterface,
 
         self.csv_file = os.path.join(self.tempdir, 'testing.csv')
 
-        write_csv_file(self.csv_file)
+        write_csv_file(self.csv_file, GoodTable.headers, GoodTable.records)
 
         self.addTypeEqualityFunc(CSVApplication, self.assertApplicationEqual)
         self.addTypeEqualityFunc(CSVApplicationPolicy,
@@ -84,3 +115,18 @@ class TestCSVAccounting(TempMixin, ABCTestDatabaseInterface,
 
         user = accounting.get_user_by_name('unknown')
         self.assertIsNone(user)
+
+    def test_error_with_missing_headers(self):
+        write_csv_file(self.csv_file,
+                       BadTableMissingHeaders.headers,
+                       BadTableMissingHeaders.records)
+
+        with self.assertRaisesRegex(ValueError, "Missing headers"):
+            self.create_accounting()
+
+    def test_load_csv_file_with_good_headers_different_order(self):
+        """ Test loading a good table with headers of different orders """
+        write_csv_file(self.csv_file,
+                       GoodTableWithDifferentHeaders.headers,
+                       GoodTableWithDifferentHeaders.records)
+        self.create_accounting()
