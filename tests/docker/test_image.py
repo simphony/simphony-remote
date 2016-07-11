fea64721e2f6c541690de75079733ddbe9a87d42
Kit Yan Choi <kchoi@enthought.com>
2016-07-11 17:11:07 +0100

    add test for from_docker_dict using dictionary format from inspect_image

--- tests/docker/test_image.py
+++ tests/docker/test_image.py
@@ -6,7 +6,7 @@ from tests.utils import mock_docker_client
 
 
 class TestImage(TestCase):
-    def test_from_docker_dict(self):
+    def test_from_docker_dict_images(self):
         docker_client = mock_docker_client()
         image_dict = docker_client.images()[0]
         image = Image.from_docker_dict(image_dict)
@@ -17,3 +17,17 @@ class TestImage(TestCase):
                          image_dict["Labels"][docker_labels.DESCRIPTION])
         self.assertEqual(image.ui_name,
                          image_dict["Labels"][docker_labels.UI_NAME])
+
+    def test_from_docker_dict_inspect_image(self):
+        docker_client = mock_docker_client()
+        image_dict = docker_client.inspect_image()
+        image = Image.from_docker_dict(image_dict)
+
+        self.assertEqual(image.docker_id, image_dict["Id"])
+        self.assertEqual(image.name, image_dict["RepoTags"][0])
+        self.assertEqual(
+            image.description,
+            image_dict['Config']["Labels"][docker_labels.DESCRIPTION])
+        self.assertEqual(
+            image.ui_name,
+            image_dict['Config']["Labels"][docker_labels.UI_NAME])
