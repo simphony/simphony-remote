06893caec9922f8920cafbd54c3ae5237fe16334
Stefano Borini <sborini@enthought.com>
2016-07-12 19:21:25 +0100

    Added comment logic for Container resource

--- remoteappmanager/restmodel/container.py
+++ remoteappmanager/restmodel/container.py
@@ -3,7 +3,35 @@ from tornado import gen
 
 
 class Container(Resource):
-    @classmethod
+    @gen.coroutine
+    def create(self, representation):
+        # This should create the container.
+        # the representation should accept the application mapping id we
+        # want to start
+        pass
+
+    @gen.coroutine
+    def retrieve(self, identifier):
+        # We should return the representation of the running container
+        # with its current status
+        pass
+
+
+    @gen.coroutine
+    def update(self, identifier, representation):
+        # Used to change the status. We should be able to say status=stop.
+        # and this will stop the container
+        pass
+
+
+    @gen.coroutine
+    def delete(self, identifier):
+        # This should stop the container. Maybe we should skip the status
+        # modification, and just use delete to stop a container?
+        pass
+
     @gen.coroutine
     def items(self):
+        # We should return the list of containers we are currently
+        # running.
         return []
