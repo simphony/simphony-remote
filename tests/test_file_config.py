8cce9cf201c650d591b3376d367bebfc32ebfefc
Kit Yan Choi <kchoi@enthought.com>
2016-07-13 11:25:03 +0100

    Add comment back

--- tests/test_file_config.py
+++ tests/test_file_config.py
@@ -25,6 +25,10 @@ class TestFileConfig(TempMixin, unittest.TestCase):
 
     def setUp(self):
         super().setUp()
+
+        # We can run the application config only once. It uses a global
+        # object that can't be reinitialized cleanly unless we access a private
+        # member.
         self.config_file = os.path.join(self.tempdir,
                                         'config.py')
 
