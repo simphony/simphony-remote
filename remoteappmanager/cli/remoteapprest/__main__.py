7dfe9a8537ea70d5b19ff2edf79218fa9d69f4e5
Stefano Borini <sborini@enthought.com>
2016-07-15 16:45:28 +0100

    Added a space after the id so that copy/pasting the id is easier

--- remoteappmanager/cli/remoteapprest/__main__.py
+++ remoteappmanager/cli/remoteapprest/__main__.py
@@ -132,7 +132,7 @@ def available(ctx):
                                   item_id))
         response = requests.get(request_url, cookies=cookies, verify=False)
         app_data = json.loads(response.content.decode("utf-8"))
-        print("{}: {}".format(
+        print("{} : {}".format(
             item_id, app_data["image"]
         ))
 
@@ -194,7 +194,7 @@ def running(ctx):
                                   item_id))
         response = requests.get(request_url, cookies=cookies, verify=False)
         app_data = json.loads(response.content.decode("utf-8"))
-        print("{}: {}".format(
+        print("{} : {}".format(
             item_id, app_data["image_name"]
         ))
 
