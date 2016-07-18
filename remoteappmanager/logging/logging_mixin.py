cc7853496de3ae573e666de4ca22cb7447b4b91b
Stefano Borini <sborini@enthought.com>
2016-07-18 14:08:11 +0100

    Introduced monkeypatched issue method to create issue

--- remoteappmanager/logging/logging_mixin.py
+++ remoteappmanager/logging/logging_mixin.py
@@ -1,8 +1,30 @@
+import uuid
+import types
+
 from traitlets import (
     HasTraits,
     Instance,
     default
 )
+
+
+def issue(self, message, exc=None):
+    """Accepts a message that will be logged with an additional reference
+    code for easy log lookup.
+
+    The identifier will be returned for inclusion in user-visible
+    error messages.
+    """
+    ref = str(uuid.uuid1())
+
+    if exc is None:
+        err_message = "{}. Ref: {}".format(message, ref)
+    else:
+        err_message = "{} : {}. Ref: {}".format(
+            message, str(exc), ref)
+
+    self.error(err_message)
+    return ref
 
 
 class LoggingMixin(HasTraits):
@@ -13,5 +35,9 @@ class LoggingMixin(HasTraits):
 
     @default('log')
     def _log_default(self):
-        from traitlets import log
-        return log.get_logger()
+        from tornado.log import app_log
+
+        # monkey patch the logger to provide an additional method that handles
+        # issues
+        app_log.issue = types.MethodType(issue, app_log)
+        return app_log
