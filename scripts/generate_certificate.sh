e7ba940fff8f7e01b354892b82b440f584725abd
Stefano Borini <sborini@enthought.com>
2016-07-22 14:02:50 +0100

    Made certificate generation batch operation

--- scripts/generate_certificate.sh
+++ scripts/generate_certificate.sh
@@ -8,8 +8,8 @@ then
     exit 0
 fi
 
-openssl genrsa -des3 -out test.key 1024
-openssl req -new -key test.key -out test.csr
+openssl genrsa -out test.key 1024
+openssl req -new -key test.key -out test.csr -batch
 cp test.key test.key.org
 openssl rsa -in test.key.org -out test.key
 openssl x509 -req -days 365 -in test.csr -signkey test.key -out test.crt
