c989a2f977237b7926433e16fbe824bfdc671cb3
Stefano Borini <sborini@enthought.com>
2016-07-26 09:48:18 +0100

    Working REST+JS implementation

--- remoteappmanager/static/js/home.js
+++ remoteappmanager/static/js/home.js
@@ -1,8 +1,8 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-require(["jquery", "jhapi", "remoteappapi"], 
-        function($, JHAPI, RemoteAppAPI) {
+require(["jquery", "jhapi", "utils", "remoteappapi"],
+        function($, JHAPI, utils, RemoteAppAPI) {
     "use strict";
     
     var base_url = window.apidata.base_url;
@@ -21,11 +21,20 @@ require(["jquery", "jhapi", "remoteappapi"],
     $(".start-button").click(function () {
         var id = this.id;
         appapi.start_application(id, {
-                success: function () {
-                    window.location.reload()
+                statusCode: {
+                    201: function (data, textStatus, request) {
+                        var location = request.getResponseHeader('Location');
+                        var url = utils.parse_url(location);
+                        var path = url.pathname.replace(/\/$/, "");
+                        var array = path.split('/');
+                        id = array[array.length-1];
+                        
+                        window.location = base_url+"containers/"+id
+                    }
                 }
             });
-    });
+        }
+    );
 
     $(".stop-button").click(function () {
         var id = this.id;
