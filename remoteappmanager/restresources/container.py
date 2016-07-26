2651c513034a6a8d4d61acef454fb2dce3df2b6b
Stefano Borini <sborini@enthought.com>
2016-07-26 11:02:12 +0100

    Moved http polling to netutils. Added test

--- remoteappmanager/restresources/container.py
+++ remoteappmanager/restresources/container.py
@@ -1,17 +1,14 @@
-import socket
 import os
 from datetime import timedelta
-import errno
 
-from tornado import gen, ioloop
-from tornado.httpclient import AsyncHTTPClient, HTTPError
-from tornado.log import app_log
+from tornado import gen
 
 from remoteappmanager.docker.docker_labels import SIMPHONY_NS
 from remoteappmanager.rest import exceptions
 from remoteappmanager.rest.resource import Resource
 from remoteappmanager.docker.container import Container as DockerContainer
 from remoteappmanager.utils import url_path_join
+from remoteappmanager.netutils import wait_for_http_server_2xx
 
 
 class Container(Resource):
@@ -214,42 +211,6 @@ class Container(Resource):
             url_path_join(self.application.command_line_config.base_urlpath,
                           container.urlpath))
 
-        yield _wait_for_http_server_2xx(
+        yield wait_for_http_server_2xx(
             server_url,
             self.application.file_config.network_timeout)
-
-
-@gen.coroutine
-def _wait_for_http_server_2xx(url, timeout=10):
-    """Wait for an HTTP Server to respond at url and respond with a 2xx code.
-    """
-    loop = ioloop.IOLoop.current()
-    tic = loop.time()
-    client = AsyncHTTPClient()
-
-    while loop.time() - tic < timeout:
-        try:
-            response = yield client.fetch(url, follow_redirects=True)
-        except HTTPError as e:
-            # Skip code 599 because it's expected and we don't want to
-            # pollute the logs.
-            if e.code != 599:
-                app_log.warning("Server at %s responded with: %s", url, e.code)
-        except (OSError, socket.error) as e:
-            if e.errno not in {errno.ECONNABORTED,
-                               errno.ECONNREFUSED,
-                               errno.ECONNRESET}:
-                app_log.warning("Failed to connect to %s (%s)", url, e)
-        except Exception as e:
-            # In case of any unexpected exception, we just log it and keep
-            # trying until eventually we timeout.
-            app_log.warning("Unknown exception occurred connecting to "
-                            "%s (%s)", url, e)
-        else:
-            app_log.info("Server at %s responded with: %s", url, response.code)
-            return
-
-        yield gen.sleep(0.1)
-
-    raise TimeoutError("Server at {} didn't respond in {} seconds".format(
-        url, timeout))
---
+++ tests/test_netutils.py
@@ -0,0 +1,35 @@
+from remoteappmanager.netutils import wait_for_http_server_2xx
+from tornado import web, gen
+from tornado.testing import AsyncHTTPTestCase, gen_test
+
+
+class ShortHandler(web.RequestHandler):
+    error_count = 10
+
+    def get(self):
+        if type(self).error_count:
+            type(self).error_count -= 1
+            raise web.HTTPError(500)
+
+        self.set_status(200)
+        self.write("hello")
+        self.finish()
+
+
+class LongHandler(ShortHandler):
+    error_count = 100000
+
+
+class TestUtils(AsyncHTTPTestCase):
+    def get_app(self):
+
+        app = web.Application(handlers=[('/short', ShortHandler),
+                                        ('/long', LongHandler)])
+        return app
+
+    @gen_test
+    def test_basic(self):
+        yield wait_for_http_server_2xx(self.get_url("/short"), timeout=3)
+
+        with self.assertRaises(TimeoutError):
+            yield wait_for_http_server_2xx(self.get_url("/long"), timeout=3)
