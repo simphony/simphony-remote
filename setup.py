de23251995d5092994eb0fd641f6138c1516862c
Stefano Borini <sborini@enthought.com>
2016-09-15 15:24:49 +0100

    Attempting to simply skip jhub if we are on RTD

--- setup.py
+++ setup.py
@@ -4,25 +4,36 @@ from remoteappmanager import __version__
 
 VERSION = __version__
 
+import os
+
+requirements=[
+    "setuptools>=21.0",
+    "traitlets>=4.1",
+    "tornado>=4.3",
+    "docker-py>=1.8",
+    "requests>=2.10.0",
+    "escapism>=0.0.1",
+    "jinja2>=2.8",
+    "jupyter_client>=4.3.0",
+    "click>=6.6",
+    "tabulate>=0.7.5",
+]
+
+
+# Unfortunately RTD cannot install jupyterhub because jupyterhub needs bower,
+# and that is not available. We prevent the request for the unreleased jhub
+# by skipping it if we are on RTD
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if not on_rtd:
+    requirements.append("jupyterhub>=0.7.0dev0")
+
 # main setup configuration class
 setup(
     name='remoteappmanager',
     version=VERSION,
     author='SimPhoNy Project',
     description='Remote application manager sub-executable',
-    install_requires=[
-        "setuptools>=21.0",
-        "traitlets>=4.1",
-        "tornado>=4.3",
-        "docker-py>=1.8",
-        "requests>=2.10.0",
-        "escapism>=0.0.1",
-        "jinja2>=2.8",
-        "jupyterhub>=0.7.0dev0",
-        "jupyter_client>=4.3.0",
-        "click>=6.6",
-        "tabulate>=0.7.5",
-    ],
+    install_requires=requirements,
     packages=find_packages(),
     include_package_data=True,
     entry_points={
