1a6a976aba496599b0e07dd0d343d42a86635045
Stefano Borini <sborini@enthought.com>
2016-09-21 18:09:45 +0100

    Added tests for no user case

--- remoteappmanager/restresources/tests/test_application.py
+++ remoteappmanager/restresources/tests/test_application.py
@@ -1,13 +1,13 @@
-from unittest.mock import Mock, patch
+from unittest.mock import Mock
 
 from tornado import web, escape, gen
 from tornadowebapi import registry
+from tornadowebapi.authenticator import NullAuthenticator
 from tornadowebapi.http import httpstatus
 
 from remoteappmanager.docker.container import Container
 from remoteappmanager.docker.image import Image
 from remoteappmanager.restresources import Application
-from remoteappmanager.tests import utils
 from remoteappmanager.tests.utils import AsyncHTTPTestCase, mock_coro_factory
 from remoteappmanager.tests.mocking.dummy import create_hub
 
@@ -26,10 +26,10 @@ class TestApplication(AsyncHTTPTestCase):
         super().setUp()
 
     def get_app(self):
-        reg = registry.Registry()
-        reg.register(Application)
-        reg.authenticator = DummyAuthenticator
-        handlers = reg.api_handlers('/')
+        self.reg = registry.Registry()
+        self.reg.register(Application)
+        self.reg.authenticator = DummyAuthenticator
+        handlers = self.reg.api_handlers('/')
         app = web.Application(handlers=handlers)
         app.db = Mock()
         app.hub = create_hub()
@@ -58,98 +58,84 @@ class TestApplication(AsyncHTTPTestCase):
         return app
 
     def test_items(self):
-        def prepare_side_effect(*args, **kwargs):
-            user = Mock()
-            user.account = Mock()
-            args[0].current_user = user
+        res = self.fetch("/api/v1/applications/")
 
-        with patch("remoteappmanager"
-                   ".handlers"
-                   ".base_handler"
-                   ".BaseHandler"
-                   ".prepare",
-                   new_callable=utils.mock_coro_new_callable(
-                    side_effect=prepare_side_effect)
-                   ):
-            res = self.fetch("/api/v1/applications/")
+        self.assertEqual(res.code, httpstatus.OK)
+        self.assertEqual(escape.json_decode(res.body),
+                         {"items": ["one", "two"]})
 
-            self.assertEqual(res.code, httpstatus.OK)
-            self.assertEqual(escape.json_decode(res.body),
-                             {"items": ["one", "two"]})
+        # Check if nothing is returned if no images are present
+        self._app.container_manager.image = mock_coro_factory(
+            return_value=None)
 
-            # Check if nothing is returned if no images are present
-            self._app.container_manager.image = mock_coro_factory(
-                return_value=None)
+        res = self.fetch("/api/v1/applications/")
 
-            res = self.fetch("/api/v1/applications/")
+        self.assertEqual(res.code, httpstatus.OK)
+        self.assertEqual(escape.json_decode(res.body),
+                         {"items": []})
 
-            self.assertEqual(res.code, httpstatus.OK)
-            self.assertEqual(escape.json_decode(res.body),
-                             {"items": []})
+    def test_items_no_user(self):
+        self.reg.authenticator = NullAuthenticator
+        res = self.fetch("/api/v1/applications/")
+
+        self.assertEqual(res.code, httpstatus.NOT_FOUND)
 
     def test_retrieve(self):
-        def prepare_side_effect(*args, **kwargs):
-            user = Mock()
-            user.account = Mock()
-            args[0].current_user = user
+        res = self.fetch("/api/v1/applications/one/")
 
-        with patch("remoteappmanager"
-                   ".handlers"
-                   ".base_handler"
-                   ".BaseHandler"
-                   ".prepare",
-                   new_callable=utils.mock_coro_new_callable(
-                       side_effect=prepare_side_effect)
-                   ):
-            res = self.fetch("/api/v1/applications/one/")
+        self.assertEqual(res.code, httpstatus.OK)
+        self.assertEqual(escape.json_decode(res.body),
+                         {'container': None,
+                          'image': {'description': '',
+                                    'icon_128': '',
+                                    'name': 'boo',
+                                    'ui_name': 'foo_ui',
+                                    'policy': {
+                                        "allow_home": True,
+                                        "volume_mode": 'ro',
+                                        "volume_source": "foo",
+                                        "volume_target": "bar",
+                                    }},
+                          'mapping_id': 'one'})
 
-            self.assertEqual(res.code, httpstatus.OK)
-            self.assertEqual(escape.json_decode(res.body),
-                             {'container': None,
-                              'image': {'description': '',
-                                        'icon_128': '',
-                                        'name': 'boo',
-                                        'ui_name': 'foo_ui',
-                                        'policy': {
-                                            "allow_home": True,
-                                            "volume_mode": 'ro',
-                                            "volume_source": "foo",
-                                            "volume_target": "bar",
-                                        }},
-                              'mapping_id': 'one'})
+        self._app.container_manager.containers_from_mapping_id = \
+            mock_coro_factory(return_value=[Container(
+                name="container",
+                image_name="xxx",
+                url_id="yyy")])
 
-            self._app.container_manager.containers_from_mapping_id = \
-                mock_coro_factory(return_value=[Container(
-                    name="container",
-                    image_name="xxx",
-                    url_id="yyy")])
+        res = self.fetch("/api/v1/applications/one/")
 
-            res = self.fetch("/api/v1/applications/one/")
+        self.assertEqual(res.code, httpstatus.OK)
+        self.assertEqual(escape.json_decode(res.body),
+                         {'container':
+                             {'image_name': 'xxx',
+                              'name': 'container',
+                              'url_id': 'yyy'},
+                          'image': {'description': '',
+                                    'icon_128': '',
+                                    'name': 'boo',
+                                    'ui_name': 'foo_ui',
+                                    'policy': {
+                                        "allow_home": True,
+                                        "volume_mode": 'ro',
+                                        "volume_source": "foo",
+                                        "volume_target": "bar",
+                                    }},
+                          'mapping_id': 'one'})
 
-            self.assertEqual(res.code, httpstatus.OK)
-            self.assertEqual(escape.json_decode(res.body),
-                             {'container':
-                                 {'image_name': 'xxx',
-                                  'name': 'container',
-                                  'url_id': 'yyy'},
-                              'image': {'description': '',
-                                        'icon_128': '',
-                                        'name': 'boo',
-                                        'ui_name': 'foo_ui',
-                                        'policy': {
-                                            "allow_home": True,
-                                            "volume_mode": 'ro',
-                                            "volume_source": "foo",
-                                            "volume_target": "bar",
-                                        }},
-                              'mapping_id': 'one'})
+        res = self.fetch("/api/v1/applications/three/")
 
-            res = self.fetch("/api/v1/applications/three/")
+        self.assertEqual(res.code, httpstatus.NOT_FOUND)
 
-            self.assertEqual(res.code, httpstatus.NOT_FOUND)
+        # Check the not found case if the image is not present
+        self._app.container_manager.image = mock_coro_factory(None)
 
-            # Check the not found case if the image is not present
-            self._app.container_manager.image = mock_coro_factory(None)
+        res = self.fetch("/api/v1/applications/one/")
+        self.assertEqual(res.code, httpstatus.NOT_FOUND)
 
-            res = self.fetch("/api/v1/applications/one/")
-            self.assertEqual(res.code, httpstatus.NOT_FOUND)
+    def test_retrieve_no_user(self):
+        self.reg.authenticator = NullAuthenticator
+        res = self.fetch("/api/v1/applications/one/")
+
+        self.assertEqual(res.code, httpstatus.NOT_FOUND)
