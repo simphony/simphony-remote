3bd1fedfce6a8c248c45f2fee4e38cf6c9210dcb
Stefano Borini <sborini@enthought.com>
2016-09-26 14:05:05 +0100

    Added tests

--- jstests/tests/home/test_views.js
+++ jstests/tests/home/test_views.js
@@ -17,7 +17,11 @@ define(function (require) {
                 assert.equal(applist.children().length, 2);
                 assert.equal($("#applist > div:nth-child(1) > div > h4").text(), "Application 1");
                 assert.equal($("#applist > div:nth-child(2) > div > h4").text(), "Application 2");
+            })
+            .done(function() {
+                model.app_data[0].image.ui_name = "Hello";
+                view.update_entry(0);
+                assert.equal($("#applist > div:nth-child(1) > div > h4").text(), "Hello");
             });
-        
     });
 });
--- remoteappmanager/static/js/home/configurables.js
+++ remoteappmanager/static/js/home/configurables.js
@@ -1,17 +1,8 @@
 define(["jquery"], function($) {
     "use strict";
-    
-    function viewport_resolution() {
-        // Returns the current viewport resolution as a "WxH" string
-        var e = window, a = 'inner';
-        if ( !( 'innerWidth' in window ) ) {
-            a = 'client';
-            e = document.documentElement || document.body;
-        }
-        return e[ a+'Width' ]+"x"+e[ a+'Height' ];
-    }
 
     var ResolutionModel = function () {
+        // Model for the resolution configurable.
         var self = this;
         this.resolution = "Window";
         this.resolution_options = ["Window", "1920x1080", "1280x1024", "1280x800", "1024x768"];
@@ -44,22 +35,42 @@ define(["jquery"], function($) {
     ResolutionModel.prototype.tag = "resolution";
    
     ResolutionModel.prototype.as_config_dict = function() {
+        // Returns the configuration dict to hand over to the API request.
+        // e.g. 
+        // {
+        //    "resolution" : "1024x768"
+        // }
+        // The returned dictionary must be added to the configurable
+        // parameter under the key given by the tag member. 
         var resolution = this.resolution;
         
         if (resolution === 'Window') {
-            resolution = viewport_resolution();
+            resolution = this._viewport_resolution();
         }
         
         return {
             "resolution": resolution
         };
     };
+   
+    ResolutionModel.prototype._viewport_resolution = function () {
+        // Returns the current viewport resolution as a "WxH" string
+        var e = window, a = 'inner';
+        if ( !( 'innerWidth' in window ) ) {
+            a = 'client';
+            e = document.documentElement || document.body;
+        }
+        return e[ a+'Width' ]+"x"+e[ a+'Height' ];
+    };
     
+    // Define all your configurables here.
     var configurables = {
         ResolutionModel: ResolutionModel
     };
     
     var from_tag = function (tag) {
+        // Given a tag, lookup the appropriate configurable and
+        // return it. If the tag matches no configurable, returns null
         for (var conf in configurables) {
             if (configurables[conf].prototype.tag === tag) {
                 return configurables[conf];
