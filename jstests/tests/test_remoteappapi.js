e73d1596d6b3022d46da8f15e332cbd64fa91815
Stefano Borini <sborini@enthought.com>
2016-09-27 12:36:24 +0100

    Replaced tabs with spaces

--- jstests/tests/test_remoteappapi.js
+++ jstests/tests/test_remoteappapi.js
@@ -1,12 +1,12 @@
 define(["require", "jquery"], function (require, $) {
-	var RemoteAppAPI = require("remoteappapi");
-	var mock_api_request = function(result) {
-		"use strict";
-		return $.Deferred();
-	};
-	QUnit.module("Remote App API");
-	QUnit.test("test", function (assert) { 
+    var RemoteAppAPI = require("remoteappapi");
+    var mock_api_request = function(result) {
+        "use strict";
+        return $.Deferred();
+    };
+    QUnit.module("Remote App API");
+    QUnit.test("test", function (assert) {
         var api = new RemoteAppAPI();
         assert.ok(api !== null);
-	});
+    });
 });
