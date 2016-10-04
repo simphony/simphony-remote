ec902d84d71c089f2097dac73198a2b9f7e804ce
Stefano Borini <stefanoborini@users.noreply.github.com>
2016-10-04 11:49:30 +0100

    Minor comments

--- remoteappmanager/environment_config.py
+++ remoteappmanager/environment_config.py
@@ -6,15 +6,18 @@ class EnvironmentConfig(HasTraits):
     """Configuration options for the application server,
     originating from environment variables."""
 
-    jpy_api_token = Unicode(help="The jupyterhub API token")
+    #: Token for JupyterHub API. Originates from JPY_API_TOKEN  
+    jpy_api_token = Unicode(help="The JupyterHub API token")
 
+    #: Token for the ReverseProxy API. Originates from 
+    #: PROXY_API_TOKEN
     proxy_api_token = Unicode(help="The Reverse Proxy API token")
 
     # Home is not part of it because we change it along the way,
     # so we can't rely on the value at startup.
 
     def parse_config(self):
-        """Parses the command line arguments, and assign their
+        """Parses the environment variables, and assign their
         values to our local traits.
         """
 
