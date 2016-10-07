819f44677c96568d96f2b37514a479a548107df5
Stefano Borini <sborini@enthought.com>
2016-10-07 13:17:34 +0100

    Extracted utility function to parse the volume string.

--- remoteappmanager/utils.py
+++ remoteappmanager/utils.py
@@ -23,3 +23,19 @@ def url_path_join(*pieces):
 def with_end_slash(url):
     """Normalises a url to have an ending slash, and only one."""
     return url.rstrip("/")+"/"
+
+
+def parse_volume_string(volume_string):
+    """Parses a volume specification string SOURCE:TARGET:MODE into
+    its components, or raises click.BadOptionUsage if not according
+    to format."""
+    try:
+        source, target, mode = volume_string.split(":")
+    except ValueError:
+        raise ValueError("Volume string must be in the form "
+                         "source:target:mode")
+
+    if mode not in ('rw', 'ro'):
+        raise ValueError("Volume mode must be either 'ro' or 'rw'")
+
+    return source, target, mode
