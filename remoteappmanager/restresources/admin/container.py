30e5d6add70ee259e53eb5c720c385b5e0169d7e
Stefano Borini <sborini@enthought.com>
2016-10-14 13:58:14 +0100

    Fix: prevent other user to delete or retrieve another user's containers.

--- remoteappmanager/restresources/admin/container.py
+++ remoteappmanager/restresources/admin/container.py
@@ -4,7 +4,6 @@ from datetime import timedelta
 from tornado import gen
 
 from tornadowebapi import exceptions
-from tornadowebapi.exceptions import NotFound
 from tornadowebapi.resource import Resource
 
 from remoteappmanager.utils import url_path_join
@@ -15,97 +14,6 @@ from remoteappmanager.restresources.decorators import authenticated
 class Container(Resource):
     @gen.coroutine
     @authenticated
-    def create(self, representation):
-        """Create the container.
-        The representation should accept the application mapping id we
-        want to start"""
-        if self.current_user is None:
-            raise NotFound()
-
-        try:
-            mapping_id = representation["mapping_id"]
-        except KeyError:
-            raise exceptions.BadRequest(message="missing mapping_id")
-
-        webapp = self.application
-        account = self.current_user.account
-        all_apps = webapp.db.get_apps_for_user(account)
-        container_manager = webapp.container_manager
-
-        choice = [(m_id, app, policy)
-                  for m_id, app, policy in all_apps
-                  if m_id == mapping_id]
-
-        if not choice:
-            self.log.warning("Could not find resource "
-                             "for mapping id {}".format(mapping_id))
-            raise exceptions.BadRequest(message="unrecognized mapping_id")
-
-        _, app, policy = choice[0]
-
-        image = yield container_manager.image(app.image)
-        if image is None:
-            raise exceptions.BadRequest(message="unrecognized image")
-
-        try:
-            environment = self._environment_from_configurables(image,
-                                                               representation)
-        except Exception:
-            raise exceptions.BadRequest(message="invalid configurables")
-
-        # Everything is fine. Start and wait for the container to come online.
-        try:
-            container = yield self._start_container(
-                self.current_user.name,
-                app,
-                policy,
-                mapping_id,
-                environment
-                )
-        except Exception as e:
-            raise exceptions.Unable(message=str(e))
-
-        try:
-                yield self._wait_for_container_ready(container)
-        except Exception as e:
-            self._remove_container_noexcept(container)
-            raise exceptions.Unable(message=str(e))
-
-        urlpath = url_path_join(
-            self.application.command_line_config.base_urlpath,
-            container.urlpath)
-
-        try:
-            yield self.application.reverse_proxy.register(
-                urlpath, container.host_url)
-        except Exception as e:
-            self._remove_container_noexcept(container)
-            raise exceptions.Unable(message=str(e))
-
-        return container.url_id
-
-    @gen.coroutine
-    @authenticated
-    def retrieve(self, identifier):
-        """Return the representation of the running container."""
-        container_manager = self.application.container_manager
-        container = yield container_manager.container_from_url_id(identifier)
-
-        if container is None:
-            self.log.warning("Could not find container for id {}".format(
-                identifier))
-            raise exceptions.NotFound()
-
-        if container.user != self.current_user.name:
-            raise exceptions.NotFound()
-
-        return dict(
-            name=container.name,
-            image_name=container.image_name
-        )
-
-    @gen.coroutine
-    @authenticated
     def delete(self, identifier):
         """Stop the container."""
         container_manager = self.application.container_manager
@@ -114,9 +22,6 @@ class Container(Resource):
         if not container:
             self.log.warning("Could not find container for id {}".format(
                              identifier))
-            raise exceptions.NotFound()
-
-        if container.user != self.current_user.name:
             raise exceptions.NotFound()
 
         urlpath = url_path_join(
@@ -138,37 +43,6 @@ class Container(Resource):
         except Exception:
             self.log.exception("Could not stop and remove container "
                                "for id {}".format(identifier))
-
-    @gen.coroutine
-    @authenticated
-    def items(self):
-        """"Return the list of containers we are currently running."""
-        container_manager = self.application.container_manager
-
-        apps = self.application.db.get_apps_for_user(
-            self.current_user.account)
-
-        running_containers = []
-
-        for mapping_id, app, policy in apps:
-            image = yield container_manager.image(app.image)
-
-            if image is None:
-                # The user has access to an application that is no longer
-                # available in docker. We just move on.
-                continue
-
-            containers = yield container_manager.containers_from_mapping_id(
-                self.current_user.name,
-                mapping_id)
-
-            # We assume that we can only run one container only (although the
-            # API considers a broader possibility for future extension.
-            if len(containers):
-                container = containers[0]
-                running_containers.append(container.url_id)
-
-        return running_containers
 
     ##################
     # Private
