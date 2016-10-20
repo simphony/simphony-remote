c4dc77ee4fa91ee3cb4e70f4b9100e06ec1c3d17
Stefano Borini <sborini@enthought.com>
2016-10-20 14:38:58 +0100

    Removes containers on request.

--- remoteappmanager/static/js/admin/containers/controller.js
+++ remoteappmanager/static/js/admin/containers/controller.js
@@ -1,18 +1,30 @@
 define([
     "jquery",
+    "bootstrap",
     "admin/tabular/controller",
     "remoteappapi"
-], function ($, tabular, RemoteAppAPI) {
+], function ($, bootstrap, tabular, RemoteAppAPI) {
     "use strict";
     var base_url = window.apidata.base_url;
     var appapi = new RemoteAppAPI(base_url);
     
-    $("button").click(
-        function() {
-            var url_id = $(this).attr("data-value");
-            appapi.stop_application(url_id, {
-                success: function () {
-                    window.location.reload();
-                }});
+    //$(".stop-container").click(function() { $(".modal").modal(); });
+    $('.modal').on('show.bs.modal', function (event) {
+      var button = $(event.relatedTarget);
+      var url_id = button.data('value');
+      var modal = $(this);
+      modal.find('.modal-title').text('Stop ' + url_id + "?");
+      modal.find('.modal-body').text(
+          "Do you want to stop the container " +
+          url_id + 
+          "? This will stop the user session in that container."
+      );
+      modal.find('.modal-footer .primary').click(
+          function() {
+              appapi.stop_application(url_id, {
+                  success: function () {
+                      window.location.reload();
+                  }});
+        });
     });
 });
