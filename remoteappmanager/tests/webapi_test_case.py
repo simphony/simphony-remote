1b0964124f00105a3b16e491c51f9d2d231c8f39
Stefano Borini <sborini@enthought.com>
2016-10-28 13:12:00 +0100

    Added docs

--- remoteappmanager/tests/webapi_test_case.py
+++ remoteappmanager/tests/webapi_test_case.py
@@ -3,7 +3,27 @@ from .utils import AsyncHTTPTestCase
 
 
 class WebAPITestCase(AsyncHTTPTestCase):
+    """Base class for tests accessing the Web API,
+    providing basic methods to issue web requests, get results, and compare
+    against the expected HTTP code.
+    """
     def get(self, url, expect_code=None):
+        """Gets a URL.
+
+        Parameters
+        ----------
+        url: str
+            the URL to GET
+        expect_code: int
+            The expected HTTP code this request should produce.
+            If the request produces a different code, the test will fail.
+
+        Returns
+        -------
+        tuple (code, data), where code is the response HTTP code
+        and data is the json decoded representation, or None if the decoding
+        fails.
+        """
 
         cookie = self.cookie_auth_token()
         kwargs = {}
@@ -24,6 +44,24 @@ class WebAPITestCase(AsyncHTTPTestCase):
         return res.code, data
 
     def post(self, url, data, expect_code=None):
+        """POST to a URL.
+
+        Parameters
+        ----------
+        url: str
+            the URL to POST
+        data: dict
+            The data to post (after json encoding).
+        expect_code: int
+            The expected HTTP code this request should produce.
+            If the request produces a different code, the test will fail.
+
+        Returns
+        -------
+        tuple (code, data), where code is the response HTTP code
+        and data is the json decoded representation, or None if the decoding
+        fails.
+        """
         cookie = self.cookie_auth_token()
         kwargs = {}
         if cookie:
@@ -49,6 +87,22 @@ class WebAPITestCase(AsyncHTTPTestCase):
         return res.code, ret_data
 
     def delete(self, url, expect_code=None):
+        """DELETE a URL.
+
+        Parameters
+        ----------
+        url: str
+            the URL to POST
+        expect_code: int
+            The expected HTTP code this request should produce.
+            If the request produces a different code, the test will fail.
+
+        Returns
+        -------
+        tuple (code, data), where code is the response HTTP code
+        and data is the json decoded representation, or None if
+        the decoding fails.
+        """
         cookie = self.cookie_auth_token()
         kwargs = {}
         if cookie:
@@ -71,4 +125,7 @@ class WebAPITestCase(AsyncHTTPTestCase):
         return res.code, ret_data
 
     def cookie_auth_token(self):
+        """Returns the cookie authorization token.
+        Reimplement in the test to pass the appropriate string.
+        If None, no cookie will be added to the request."""
         return None
