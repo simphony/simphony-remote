437641198164cc93c067f73216330b2995b8c2b8
Stefano Borini <sborini@enthought.com>
2016-11-10 17:46:59 +0000

    Added a new fake api to take over the import mechanism

---
+++ jstests/tests/home/mock_jsapi.js
@@ -0,0 +1,64 @@
+define(function () {
+    "use strict";
+    var MockApi = function () {
+        this.available_applications_info = function () {
+            return [{
+                image: {
+                    name: "app1",
+                    ui_name: "Application 1",
+                    icon_128: "",
+                    description: "description",
+                    policy: {
+                        allow_home: true,
+                        volume_source: "",
+                        volume_target: "",
+                        volume_mode: ""
+                    },
+                    configurables: [
+                        "resolution"
+                    ]
+                },
+                mapping_id: "12345"
+            },
+                {
+                    image: {
+                        name: "app2",
+                        ui_name: "Application 2",
+                        icon_128: "",
+                        description: "description",
+                        policy: {
+                            allow_home: true,
+                            volume_source: "",
+                            volume_target: "",
+                            volume_mode: ""
+                        },
+                        configurables: []
+                    },
+                    mapping_id: "67890"
+                }];
+        };
+    };
+
+    return {
+        Application:  {
+            create: function() {
+            },
+            delete: function() {
+            },
+            retrieve: function() {
+            },
+            item: function() {
+            }
+        },
+        Container:  {
+            create: function() {
+            },
+            delete: function() {
+            },
+            retrieve: function() {
+            },
+            item: function() {
+            }
+        }
+    };
+});
