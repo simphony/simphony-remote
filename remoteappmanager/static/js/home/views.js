0a74a230d10e79cbba108e4066cb94780f51c03f
Stefano Borini <sborini@enthought.com>
2016-11-14 10:43:28 +0000

    Porting functionality to new style

--- remoteappmanager/static/js/home/views.js
+++ remoteappmanager/static/js/home/views.js
@@ -1,8 +1,18 @@
 define([
     "jquery", 
-    "urlutils"
-], function ($, urlutils) {
+    "urlutils",
+    "handlebars"
+], function ($, urlutils, hb) {
     "use strict";
+
+    var templates = {
+       app_entries: hb.compile(
+           '<li class="active" data-index="{{index}}">' +
+           '<a href="#">' +
+           '  <img src="{{icon}}" class="app-icon">' +
+           '    <span>{{image_name}}</span></a>' +
+           '</li>')
+    };
 
     var ApplicationListView = function(model) { 
         // (Constructor) Represents the application list. In charge of 
@@ -63,9 +73,8 @@ define([
         };
         
         $("#applist").html(
-            '<div class="col-sm-12 text-center">' +
-            '<i class="fa fa-4x fa-spinner fa-spin" aria-hidden="true"></i>' +
-            '</div>');
+            '<li class="active"><a href="#"><i class="fa fa-spinner fa-spin"></i> <span>Loading</span></a></li>'
+        );
     };
 
     ApplicationListView.prototype.render = function () {
@@ -75,7 +84,7 @@ define([
         var applist = $("#applist");
         applist.empty();
         if (num_entries === 0) {
-            row = $('<div class="col-sm-12 text-center va"><h4>No applications found</h4></div>');
+            row = $('<li class="active"><a href="#">No applications found</li>');
             applist.append(row);
         } else {
             for (var i = 0; i < num_entries; i++) {
@@ -91,23 +100,6 @@ define([
         //     a progressive index for the entry.
         
         var app_data = this.model.app_data[index];
-        var configurables = this.model.configurables[index];
-        
-        var html_template = '' +
-            '<div class="row" data-index="{index}">' +
-            '  <img src="{icon}" class="col-sm-2 va" />' +
-            '  <div class="col-sm-7 va">' +
-            '    <h4>{image_name}</h4>' +
-            '    <div class="policy">{policy_html}</div>' +
-            '    <div class="configurables"></div>' +
-            '  </div>' +
-            '  <div class="col-sm-1 va">' +
-            '    <button id="bnx_{index}" data-index="{index}" class="{button_x_class} bnx btn"><span> <i class="fa {button_x_icon} x-icon"></i> {button_x_text}</span></button>' +
-            '  </div>' +
-            '  <div class="col-sm-1 va" style="padding-left: 30px">' + 
-            '    <button id="bny_{index}" data-index="{index}" class="stop-button btn-danger bny btn" style="{button_y_style}"><span><i class="fa fa-stop y-icon" aria-hidden="true"></i> Stop</span></button>' +
-            '  </div>' +
-            '</div>';
 
         var icon = app_data.image.icon_128 ?
             "data:image/png;base64,"+app_data.image.icon_128 :
@@ -115,46 +107,15 @@ define([
                 "static", "images", "generic_appicon_128.png");
 
         var image_name = app_data.image.ui_name ? app_data.image.ui_name : app_data.image.name;
-        var policy_html = this._policy_html(app_data.image.policy);
-        var cls, text, stop_style, x_icon;
-        if (app_data.container !== null) {
-            cls = "view-button btn-success";
-            text = " View";
-            x_icon = "fa-eye";
-            stop_style = "";
-        } else {
-            cls = "start-button btn-primary";
-            x_icon = "fa-play";
-            text = " Start";
-            stop_style = 'visibility: hidden;';
-        }
+
+        var row = templates.app_entries({
+            icon: icon,
+            image_name: image_name,
+            index: index
+        });
         
-        var row = $(html_template
-            .replace(/\{icon\}/g, icon)
-            .replace(/\{image_name\}/g, image_name)
-            .replace(/\{policy\}/g, policy_html)
-            .replace(/\{index\}/g, index)
-            .replace(/\{button_x_class\}/g, cls)
-            .replace(/\{button_x_icon\}/g, x_icon)
-            .replace(/\{button_x_text\}/g, text)
-            .replace(/\{button_y_style\}/g, stop_style)
-            .replace(/\{policy_html\}/g, policy_html));
-         
-        row.find(".bnx").click(this._x_button_clicked);
-        row.find(".bny").click(this._y_button_clicked);
-        
-        if (app_data.container === null) {
-            var ul = $("<ul>");
-            Object.getOwnPropertyNames(configurables).forEach(
-                function(val, idx, array) {  // jshint ignore:line
-                    var widget = configurables[val].view();
-                    ul.append($("<li>").append(widget));
-                }
-            );
-            row.find(".configurables").append(ul);
-        }
-        
-        return row;
+        var jq_row = $(row);
+        return jq_row;
     };
     
     ApplicationListView.prototype.update_entry = function (index) {
@@ -164,25 +125,6 @@ define([
         $("#applist")
             .find(".row[data-index='"+index+"']")
             .replaceWith(row);
-    };
-    
-    ApplicationListView.prototype._policy_html = function(policy) {
-        var mount_html = '';
-
-        if (policy.allow_home) {
-            mount_html += "<li>Workspace</li>";
-        }
-        if (policy.volume_source && policy.volume_target && policy.volume_mode) {
-            mount_html += "<li>"+ policy.volume_source +
-                " &#x2192; " +
-                policy.volume_target +
-                " " +
-                "(" + policy.volume_mode + ")</li>";
-        }
-        if (mount_html !== '') {
-            mount_html = "<ul>" + mount_html + "</ul>";
-        }
-        return mount_html;
     };
 
     return {
