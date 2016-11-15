8a0f3ceb3491835963e5406704d717848c28cc0a
Stefano Borini <sborini@enthought.com>
2016-11-15 14:10:43 +0000

    Streamlined ApplicationListView.

--- remoteappmanager/static/js/home/views/application_list_view.js
+++ remoteappmanager/static/js/home/views/application_list_view.js
@@ -2,15 +2,25 @@ define([
     "jquery", 
     "urlutils",
     "handlebars",
-    "utils"
-], function ($, urlutils, hb, utils) {
+], function ($, urlutils, hb) {
     "use strict";
+    
+    hb.registerHelper('icon_src', function(app_data) {
+        var icon_data = app_data.image.icon_128;
+        return (icon_data ? "data:image/png;base64,"+icon_data :
+                urlutils.path_join(this.base_url, "static", "images", "generic_appicon_128.png"))
+        }
+    );
+    hb.registerHelper('image_name', function(app_data) {
+        return (app_data.image.ui_name ? app_data.image.ui_name : app_data.image.name);
+    });
+    
     var templates = {
        app_entries: hb.compile(
-           '<li class="active" data-index="{{index}}">' +
+           '<li data-index="{{index}}">' +
            '<a href="#">' +
-           '  <img src="{{icon}}" class="app-icon">' +
-           '    <span>{{image_name}}</span></a>' +
+           '  <img src="{{icon_src app_data}}" class="app-icon">' +
+           '    <span>{{image_name app_data}}</span></a>' +
            '</li>')
     };
 
@@ -27,10 +37,14 @@ define([
         self.base_url = window.apidata.base_url;
 
         $("#applist").html(
-            '<li class="active"><a href="#"><i class="fa fa-spinner fa-spin"></i> <span>Loading</span></a></li>'
+            '<li><a href="#"><i class="fa fa-spinner fa-spin"></i> <span>Loading</span></a></li>'
         );
     };
 
+    ApplicationListView.prototype.entry_clicked = function(index) {
+        // handler when entry has been clicked. Override in controller.
+    };
+    
     ApplicationListView.prototype.render = function () {
         // Renders the full application list and adds it to the DOM.
         var num_entries = this.model.app_data.length;
@@ -38,53 +52,45 @@ define([
         var applist = $("#applist");
         applist.empty();
         if (num_entries === 0) {
-            row = $('<li class="active"><a href="#">No applications found</li>');
+            row = $('<li><a href="#">No applications found</a></li>');
             applist.append(row);
         } else {
             for (var i = 0; i < num_entries; i++) {
-                row = this.render_applist_entry(i);
+                row = this._render_applist_entry(i);
                 applist.append(row.hide().fadeIn(500));
             }
         }
     };
-    
-    ApplicationListView.prototype.render_applist_entry = function (index) {
+
+    ApplicationListView.prototype.update_entry = function (index) {
+        // Re-renders the entry for a given index, replacing the
+        // current entry.
+        var row = this._render_applist_entry(index);
+        $("#applist")
+            .find(".li[data-index='"+index+"']")
+            .replaceWith(row);
+    };
+
+
+    ApplicationListView.prototype._render_applist_entry = function (index) {
         // Returns a HTML snippet for a single application entry
         // index: 
         //     a progressive index for the entry.
         var self = this;
         var app_data = this.model.app_data[index];
 
-        var icon = app_data.image.icon_128 ?
-            "data:image/png;base64,"+app_data.image.icon_128 :
-            urlutils.path_join(this.base_url,
-                "static", "images", "generic_appicon_128.png");
-
-        var image_name = app_data.image.ui_name ? app_data.image.ui_name : app_data.image.name;
-
         var row = templates.app_entries({
-            icon: icon,
-            image_name: image_name,
-            index: index
+            index: index,
+            app_data: app_data
         });
         
         var jq_row = $(row);
         jq_row.click(function() {
-            self.model.selected_index = $(this).attr("data-index");
+            self.entry_clicked($(this).attr("data-index"));
         });
         return jq_row;
     };
    
-    ApplicationListView.prototype.update_entry = function (index) {
-        // Re-renders the entry for a given index, replacing the
-        // current entry.
-        var row = this.render_applist_entry(index);
-        $("#applist")
-            .find(".row[data-index='"+index+"']")
-            .replaceWith(row);
-        this._fill_central_area(index, false);
-    };
-    
     return {
         ApplicationListView : ApplicationListView
     };
