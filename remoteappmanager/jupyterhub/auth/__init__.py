29239db60e0ee9c594f3fbe8beaf469a83436921
Stefano Borini <sborini@enthought.com>
2017-03-06 16:53:09 +0000

    Removed flake testing from init module

--- remoteappmanager/jupyterhub/auth/__init__.py
+++ remoteappmanager/jupyterhub/auth/__init__.py
@@ -1,2 +1,2 @@
-from .world_authenticator import WorldAuthenticator
-from .github_whitelist_authenticator import GitHubWhitelistAuthenticator
+from .world_authenticator import WorldAuthenticator  # noqa
+from .github_whitelist_authenticator import GitHubWhitelistAuthenticator  # noqa
