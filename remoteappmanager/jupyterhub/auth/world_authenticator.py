f11b500966a39b86cd53abd3a8786598597ae73e
Stefano Borini <sborini@enthought.com>
2017-03-06 13:46:11 +0000

    changed quotes

--- remoteappmanager/jupyterhub/auth/world_authenticator.py
+++ remoteappmanager/jupyterhub/auth/world_authenticator.py
@@ -7,7 +7,7 @@ from jupyterhub.auth import Authenticator
 
 
 class WorldAuthenticator(Authenticator):
-    ''' This authenticator authenticates everyone '''
+    """ This authenticator authenticates everyone """
 
     @gen.coroutine
     def authenticate(self, handler, data):
