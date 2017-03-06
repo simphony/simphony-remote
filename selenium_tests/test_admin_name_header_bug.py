2de1dd11a70be9b0a6bdc805823e8d09e6b989e1
Stefano Borini <sborini@enthought.com>
2017-03-06 17:07:35 +0000

    trying getting Show without xpath (which apparently has issues)

--- selenium_tests/test_admin_name_header_bug.py
+++ selenium_tests/test_admin_name_header_bug.py
@@ -11,17 +11,11 @@ class TestAdminNameHeaderBug(SeleniumTestBase):
         driver.find_element_by_id("password_input").send_keys("admin")
         driver.find_element_by_id("login_submit").click()
         driver.find_element_by_link_text("Users").click()
-        driver.find_element_by_xpath("(//a[contains(text(),'Show')])[2]").click()
-        for i in range(60):
-            try:
-                if ("admin" == driver.find_element_by_css_selector(
-                        "span.hidden-xs").text):
-                    break
-            except:
-                pass
-            time.sleep(1)
-        else:
-            self.fail("time out")
+        driver.find_element_by_link_text("Show").click()
+        self.wait_for(lambda:
+            "admin" == driver.find_element_by_css_selector(
+                "span.hidden-xs").text
+        )
         driver.find_element_by_css_selector("span.hidden-xs").click()
         driver.find_element_by_css_selector("i.fa.fa-sign-out").click()
         driver.find_element_by_id("password_input").clear()
