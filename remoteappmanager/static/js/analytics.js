606753ac58b862cf934708c4865433f8432a4acd
Stefano Borini <sborini@enthought.com>
2017-03-28 19:10:20 +0100

    Migrating and isolating google analytics to tolerate failure to load

--- remoteappmanager/static/js/analytics.js
+++ remoteappmanager/static/js/analytics.js
@@ -7,23 +7,14 @@ define([
         var module;
         
         if (window.apidata.analytics !== undefined) {
-            window.GoogleAnalyticsObject = "ga";
-            window.ga = function () {
-                (window.ga.q = window.ga.q || []).push(arguments);
-            };
-            window.ga.l = 1 * new Date();
-            module = function () {
-                window.ga.apply(this, arguments);
-            };
-            require(["//www.google-analytics.com/analytics.js"]);
             window.ga('create', window.apidata.analytics.tracking_id, 'auto');
         } else {
             window.ga = function () {
             };
-            module = function () {
-                window.ga.apply(this, arguments);
-            };
         }
+        module = function () {
+            window.ga.apply(this, arguments);
+        };
         return module;
     } 
 
