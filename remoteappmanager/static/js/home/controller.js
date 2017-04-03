f1338a2dab0c95ee85a420d0052dc595f5f91554
martinRenou <martin.renou@isae.fr>
2017-04-03 12:42:42 +0100

    Add loading process to the ember component

--- remoteappmanager/static/js/home/controller.js
+++ remoteappmanager/static/js/home/controller.js
@@ -9,7 +9,9 @@ require([
     "home/views/application_view",
     "jsapi/v1/resources",
     "handlebars",
-    "init"
+    "init",
+    'utils',
+    'home/configurables',
 ], function(
     $,
     urlutils,
@@ -20,11 +22,90 @@ require([
     application_view,
     resources,
     hb,
-    init) {
+    init,
+    utils,
+    configurables) {
     "use strict";
 
     var ga = analytics.init();
     init.handlebars();
+
+    var available_applications_info = function () {
+        // Retrieve information from the various applications and
+        // connect the cascading callbacks.
+        // Returns a single promise. When resolved, the attached
+        // callbacks will be passed an array of the promises for the various
+        // retrieve operations, successful or not.
+        // This routine will go away when we provide the representation data
+        // inline with the items at tornado-webapi level.
+
+        var promise = $.Deferred();
+
+        resources.Application.items()
+            .done(function (ids) {
+                // We neutralize the potential error from a jXHR request
+                // and make sure that all our requests "succeed" so that
+                // all/when can guarantee everything is done.
+
+                // These will go out of scope but they are still alive
+                // and performing to completion
+                var requests = [];
+                for (var i = 0; i < ids.length; i++) {
+                    requests.push($.Deferred());
+                }
+
+                // We need to bind with the current i index in the loop
+                // Hence we create these closures to grab the index for
+                // each new index
+                var done_callback = function(index) {
+                    return function(rep) {
+                        requests[index].resolve(rep);
+                    };
+                };
+                var fail_callback = function(index) {
+                    return function() {
+                        requests[index].resolve(null);
+                    };
+                };
+
+                for (i = 0; i < ids.length; i++) {
+                    var id = ids[i];
+                    resources.Application.retrieve(id)
+                        .done(done_callback(i))
+                        .fail(fail_callback(i));
+
+                }
+
+                utils.all(requests)
+                    .done(function (promises) {
+                        // Fills the local application model with the results of the
+                        // retrieve promises.
+                        var data = [];
+                        for (var i = 0; i < promises.length; i++) {
+                            var result = promises[i];
+                            if (result !== null) {
+                                data.push(result);
+                            }
+                        }
+                        promise.resolve(data);
+                    });
+
+            })
+            .fail(function() {
+                promise.resolve([]);
+            });
+
+        return promise;
+    };
+
+    var Status = {
+        RUNNING: "RUNNING",
+        STARTING: "STARTING",
+        STOPPING: "STOPPING",
+        STOPPED: "STOPPED"
+    };
+
+    // Ember objects:
 
     var AppList = Ember.Application.create({
         LOG_TRANSITIONS: true, // For debug
@@ -35,15 +116,55 @@ require([
         init: function() {
             this._super(...arguments);
 
-            this.set('list_loading', true)
+            this.set('list_loading', true);
+            this.set('app_data', null);
+            this.set('configurables', []);
+            this.set('status', [])
 
             this.fill_list();
         },
 
         fill_list: function() {
-            setTimeout(function() {
+            return $.when(
+                available_applications_info()
+            ).done(function (app_data) {
+                this.set('app_data', app_data);
+
+                // Add the options for some image types
+                for (var i = 0; i < this.get('app_data').length; ++i) {
+                    this._update_image(i);
+                }
+
                 this.set('list_loading', false);
-            }.bind(this), 5000)
+            }.bind(this));
+        },
+
+        _update_image: function(index) {
+            // Updates the configurables submodel for a given application index.
+            var image = this.get('app_data')[index].image;
+            var app_data = this.get('app_data')[index];
+
+            this.get('configurables')[index] = {};
+
+            for (var i = 0; i < image.configurables.length; ++i) {
+                var tag = image.configurables[i];
+
+                // If this returns null, the tag has not been recognized
+                // by the client. skip it and let the server deal with the
+                // missing data, either by using a default or throwing
+                // an error.
+                var ConfigurableCls = configurables.from_tag(tag);
+
+                if (ConfigurableCls !== null) {
+                    this.get('configurables')[index][tag] = new ConfigurableCls();
+                }
+            }
+
+            if (app_data.container === null) {
+                this.get('status')[index] = Status.STOPPED;
+            } else {
+                this.get('status')[index] = Status.RUNNING;
+            }
         }
     });
 
