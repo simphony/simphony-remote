b951fb2f23cd908e8ee49157a4b2fa45739f802c
martinRenou <martin.renou@isae.fr>
2017-04-10 12:29:31 +0100

    Create hbs files containing templates of components

--- remoteappmanager/static/js/home/controller.js
+++ remoteappmanager/static/js/home/controller.js
@@ -1,49 +1,72 @@
 /*globals: require, console*/
 require([
-    "home/components/application_list_component",
-    "home/components/application_view_component",
-    "home/helpers",
+    'jquery',
+    'utils',
+    'urlutils',
+    'home/components/application_list_component',
+    'home/components/application_view_component',
+    'home/helpers'
 ], function(
+    $,
+    utils,
+    urlutils,
     application_list_component,
     application_view_component,
     helpers) {
-    "use strict";
+    'use strict';
     var Status = application_list_component.Status;
 
-    // Ember Application
+    var load_template = function(name) {
+        var template_location = urlutils.path_join(
+            window.apidata.base_url,
+            'static/js/home/templates',
+            name + '.hbs'
+        );
+        var template_name = name === 'application' ? name: 'components/' + name;
+        return $.get(template_location, function(template) {
+            Ember.TEMPLATES[template_name] = Ember.Handlebars.compile(template);
+        });
+    }
 
-    var AppList = Ember.Application.create({
-        rootElement: '#ember_container'
-    });
+    // Load templates
+    var load_templates = [
+        load_template('application'),
+        load_template('application-list'),
+        load_template('application-view')
+    ];
 
-    // Add Ember helpers to the application
+    utils.all(load_templates).done(function() {
+        // Ember Application
+        var AppList = Ember.Application.create({
+            rootElement: '#ember_container'
+        });
 
-    Object.keys(helpers).forEach(function(key) {
-        AppList[key] = helpers[key];
-    });
+        // Add Ember helpers to the application
+        Object.keys(helpers).forEach(function(key) {
+            AppList[key] = helpers[key];
+        });
 
-    // Ember Controller
+        // Ember Controller
+        AppList.ApplicationController = Ember.Controller.extend({
+            init: function() {
+                this._super(arguments);
 
-    AppList.ApplicationController = Ember.Controller.extend({
-        init: function() {
-            this._super(arguments);
+                this.set('current_application', null);
+                this.set('delayed', true);
+            },
 
-            this.set('current_application', null);
-            this.set('delayed', true);
-        },
-
-        actions: {
-            toggle_app_selected: function(application) {
-                this.set('delayed', application.status !== Status.RUNNING);
-                this.set('current_application', application);
+            actions: {
+                toggle_app_selected: function(application) {
+                    this.set('delayed', application.status !== Status.RUNNING);
+                    this.set('current_application', application);
+                }
             }
-        }
+        });
+
+        AppList.ApplicationListComponent =
+            application_list_component.ApplicationListComponent;
+
+        AppList.ApplicationViewComponent =
+            application_view_component.ApplicationViewComponent;
     });
-
-    AppList.ApplicationListComponent =
-        application_list_component.ApplicationListComponent;
-
-    AppList.ApplicationViewComponent =
-        application_view_component.ApplicationViewComponent;
-
 });
