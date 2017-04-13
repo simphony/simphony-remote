480c8b93ea2e98d4690765eae66d51e87c3ca617
martinRenou <martin.renou@isae.fr>
2017-04-13 10:37:36 +0100

    _

--- remoteappmanager/application.py
+++ remoteappmanager/application.py
@@ -1,8 +1,8 @@
 from tornado import web
+import os
 
 from remoteappmanager.base_application import BaseApplication
-from remoteappmanager.handlers.api import (
-    HomeHandler, RegisterContainerHandler)
+from remoteappmanager.handlers.api import RegisterContainerHandler
 from remoteappmanager.utils import url_path_join, without_end_slash
 from remoteappmanager import webapi
 
@@ -20,7 +20,19 @@ class Application(BaseApplication):
             (without_end_slash(
                 url_path_join(base_urlpath, "containers", "([a-z0-9_]*)")
             )+"/?", RegisterContainerHandler),
-            (base_urlpath, HomeHandler),
+            (
+                url_path_join(base_urlpath, '(.*)'),
+                web.StaticFileHandler,
+                {'path': os.path.join(
+                    os.path.dirname(
+                        os.path.abspath(__file__),
+                    ),
+                    'static',
+                    'dist'
+                ),
+                    'default_filename': 'index.html'
+                }
+            ),
             (base_urlpath.rstrip('/'), web.RedirectHandler, {
                 "url": base_urlpath}),
         ]
