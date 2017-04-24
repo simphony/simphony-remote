08c22a717e3d96421538831f303534893493446e
martinRenou <martin.renou@isae.fr>
2017-04-24 15:07:13 +0100

    Fix selenium tests

--- selenium_tests/selenium_test_base.py
+++ selenium_tests/selenium_test_base.py
@@ -112,7 +112,8 @@ class SeleniumTestBase(unittest.TestCase):
             driver = self.driver
             self.wait_for(lambda:
                           driver.find_element_by_css_selector(
-                              "#loading-spinner").style.display == "none")
+                              "#loading-spinner").value_of_css_property(
+                                  'display') == "none")
 
             self.click_by_css_selector("#applist > li > a > img")
             self.click_by_css_selector(".start-button")
