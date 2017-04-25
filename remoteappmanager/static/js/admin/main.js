4aeefcd5d1515494e94c26f3b1b8ce2a59941952
Stefano Borini <sborini@enthought.com>
2017-04-25 11:26:43 +0100

    Working

--- remoteappmanager/static/js/admin/main.js
+++ remoteappmanager/static/js/admin/main.js
@@ -7,21 +7,34 @@ require([
 
   // install router
   Vue.use(VueRouter);
-  var MainView = {
-    template: "hello"
-  };
+  
+  // 1. Define route components.
+// These can be imported from other files
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
 
-  var router = new VueRouter({
-    routes: [
-      {
-        path: '/',
-        component: MainView
-      }
-    ]
-  });
+// 2. Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// Vue.extend(), or just a component options object.
+// We'll talk about nested routes later.
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
 
-  var app = new Vue({
-    router
-  }).$mount("#adminapp");
-    
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes // short for routes: routes
+})
+
+// 4. Create and mount the root instance.
+// Make sure to inject the router with the router option to make the
+// whole app router-aware.
+const app = new Vue({
+  router
+}).$mount('#app')
+
 });
