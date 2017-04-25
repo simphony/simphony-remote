43bfd89e985bbf6eb9a9583519208632b1d055ab
martinRenou <martin.renou@isae.fr>
2017-04-25 17:15:53 +0100

    Add model to app_view

--- remoteappmanager/static/js/home/controller.js
+++ remoteappmanager/static/js/home/controller.js
@@ -29,12 +29,9 @@ require([
     // This model keeps the retrieved content from the REST query locally.
     // It is only synchronized at initial load.
     var model = new models.ApplicationListModel();
-    var app_list_view = new application_list_view.ApplicationListView(
-        { model: model }
-    );
-    var app_view = new application_view.ApplicationView(
-        { model: model }
-    );
+
+    var app_list_view = new application_list_view.ApplicationListView();
+    var app_view = new application_view.ApplicationView();
 
     /*// Temporary solution, when ApplicationView will be a Vue Object the render
     // will be automatically triggered
@@ -119,6 +116,9 @@ require([
 
     $.when(model.update()).done(function () {
         app_list_view.loading = false;
+
+        app_list_view.model = model;
+        app_view.model = model;
 
         //app_view.render(false, 200);
     });
