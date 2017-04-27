5f7024cdbbf0dc6928530d2de241519095e9920e
Stefano Borini <sborini@enthought.com>
2017-04-27 12:14:15 +0100

    temporary commit

--- remoteappmanager/static/js/admin/vue-components/UsersView.js
+++ remoteappmanager/static/js/admin/vue-components/UsersView.js
@@ -3,5 +3,43 @@ define([
 ], function(Vue) {
   "use strict";
 
-  return { template: '<div>users</div>' };
+  return {
+    template: `
+<div class="row">
+  <div class="col-md-12">
+    <div class="box">
+      <div class="box-header with-border">Users</div>
+      <div class="box-body">
+        <table id="datatable" class="display dataTable">
+          <thead>
+          <tr>
+              <th>ID</th>
+              <th>Username</th>
+              <th>Actions</th>
+          </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{{ u.id }}</td>
+              <td>{{ u.name }}</td>
+              <td><a href="{{ base_url }}users/{{ u.id }}/accounting/">Show</a></td>
+              <td><button class="btn btn-danger"
+                          data-value="{{ u.id }}"
+                          data-name="{{ u.name }}"
+                          data-toggle="modal"
+                          data-target="#action-dialog">Remove</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>`,
+    data: function () {
+      return {};
+    },
+    mounted: function () {
+    }
+  };
 });
