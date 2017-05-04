fa76ab908fc512da7c8277e6449861f02124addb
martinRenou <martin.renou@isae.fr>
2017-05-04 11:12:32 +0100

    Fix tests

--- jstests/testsuite.js
+++ jstests/testsuite.js
@@ -20,12 +20,11 @@
     });
 
     require([
-        "tests/home/test_configurables.js",
         "tests/home/test_models.js",
         "tests/home/test_views.js",
         "tests/test_utils.js",
         "tests/test_analytics.js"
-        ], function(init) {
+        ], function() {
             window.apidata = {
                 base_url: "/",
                 prefix: "/"
