8cc03d5020cffd31f366ff20dd8f70dc0cba95a2
martinRenou <martin.renou@isae.fr>
2017-05-09 15:00:13 +0100

    Fix test

--- jstests/tests/home/test_views.js
+++ jstests/tests/home/test_views.js
@@ -8,8 +8,7 @@ define([
 
     QUnit.module("home.app_list_view");
     QUnit.test("rendering list", function (assert) {
-        var update_done = assert.async();
-        var clear_done = assert.async();
+        var done = assert.async();
 
         var model = new models.ApplicationListModel();
         var app_list_view = new application_list_view.ApplicationListView({
@@ -32,24 +31,23 @@ define([
                 app_list_view.$el.querySelector("#applistentries").children.length,
                 model.app_list.length + 2
             )
-            update_done();
+
+            // Clear the model's application list
+            model.app_list = [];
+
+            Vue.nextTick(function() {
+                assert.equal(
+                    app_list_view.$el.querySelector("#applistentries > li > a").style.display,
+                    ""
+                )
+
+                assert.equal(
+                    app_list_view.$el.querySelector("#applistentries").children.length,
+                    2
+                )
+
+                done();
+            });
         })});
-
-        // Clear the model's application list
-        model.app_list = [];
-
-        Vue.nextTick(function() {
-            assert.equal(
-                app_list_view.$el.querySelector("#applistentries > li > a").style.display,
-                ""
-            )
-
-            assert.equal(
-                app_list_view.$el.querySelector("#applistentries").children.length,
-                2
-            )
-
-            clear_done();
-        });
     });
 });
