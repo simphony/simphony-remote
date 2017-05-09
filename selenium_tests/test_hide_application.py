c61f65a58250d847dbc92bcf40159cc110a6d28c
martinRenou <martin.renou@isae.fr>
2017-05-09 10:56:58 +0100

    Fix selenium tests

--- selenium_tests/test_hide_application.py
+++ selenium_tests/test_hide_application.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from selenium_tests.selenium_test_base import SeleniumTestBase
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class TestHideApplication(SeleniumTestBase):
@@ -16,8 +19,9 @@ class TestHideApplication(SeleniumTestBase):
             search_box.clear()
             search_box.send_keys('foobarheho')
 
-            self.wait_for(
-                lambda:
-                driver.find_element_by_css_selector(
-                    "#applistentries > li").value_of_css_property(
-                        'display') == "none")
+            WebDriverWait(driver, 3).until(
+                EC.invisibility_of_element_located((
+                    By.CSS_SELECTOR, '#applistentries > li')))
+
+            self.assertFalse(self.is_element_present(
+                By.CSS_SELECTOR, '#applistentries > li'))
