e9d7178c54c289e788b7a59f45c16d0477e3603a
Stefano Borini <sborini@enthought.com>
2017-05-09 17:37:36 +0100

    Added toolkit of useful entities

---
+++ remoteappmanager/static/js/admin/vue-components/toolkit/ConfirmDialog.js
@@ -0,0 +1,40 @@
+define([
+  "components/vue/dist/vue"
+], function(Vue) {
+  "use strict";
+  
+  return {
+    props: {
+      title: {
+        type: String,
+        default: "Confirm"
+      },
+      closeCallback: {
+        type: Function,
+        default: function() {}
+      },
+      okCallback: {
+        type: Function,
+        default: function() {}
+      },
+      errorMessage: {
+        type: String,
+        default: ""
+      }
+    },
+    template: '<transition name="modal">' +
+    '  <div class="modal-mask">' +
+    '    <div class="modal-wrapper">' +
+    '      <div class="modal-container">' +
+    '        <div class="modal-header"><slot name="header"><h4>{{ title }}</h4></slot></div>' +
+    '        <div class="modal-body"><slot></slot></div>' +
+    '        <div class="modal-footer text-right">' +
+    '          <button type="button" class="btn btn-default" @click="closeCallback">Cancel</button>' +
+    '          <button class="btn btn-primary primary" @click="okCallback">Ok</button>' +
+    '        </div>' +
+    '      </div>' +
+    '    </div>' +
+    '  </div>' +
+    '</transition>'
+  };
+});
---
+++ remoteappmanager/static/js/admin/vue-components/toolkit/DataTable.js
@@ -0,0 +1,53 @@
+define([
+  "components/vue/dist/vue"
+], function(Vue) {
+  "use strict";
+
+  return {
+    props: [
+      "headers", "rows", "globalActions", "rowActions"
+    ],
+    methods: {
+      isBoolean: function(value) {
+        return typeof(value) === "boolean";
+      },
+      buttonClassFromType: function(value) { 
+        var cls = {"btn": true};
+        if (value === undefined) {
+          value = "danger";
+        }
+        cls["btn-" + value] = true;
+        return cls;
+      }
+    },
+    template:
+    '<div>' +
+    '  <div class="pull-right">' +
+    '    <button v-for="action in globalActions" class="btn btn-primary" @click="action.callback">{{action.label}}</button>' +
+    '  </div>' +
+    '  <table id="datatable" class="display dataTable">' +
+    '    <thead>' +
+    '    <tr>' +
+    '        <th v-for="header in headers">{{header}}</th>' +
+    '        <th v-if="rowActions.length > 0">Actions</th>' +
+    '    </tr>' +
+    '    </thead>' +
+    '    <tbody>' +
+    '      <tr v-for="(row, row_index) in rows">' +
+    '        <template v-for="(value, col_index) in row">' +
+    '          <td v-if="isBoolean(value)"><i class="fa fa-check" v-if="value"></i></td>' +
+    '          <td v-else>{{value}}</td>' +
+    '        </template>' +
+    '        <td>' +
+    '          <button v-for="action in rowActions" ' +
+    '                   :class="buttonClassFromType(action.type)"' +
+    '                   style="margin-right: 10px"' +
+    '                   @click="action.callback(row)">{{action.label}}</button>' +
+    '        </td>' +
+    '      </tr>' +
+    '    </tbody>' +
+    '  </table>' +
+    '</div>'
+  };
+
+});
