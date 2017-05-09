291f564050f829c4152e4eb20f6bcdbd3689ff98
martinRenou <martin.renou@isae.fr>
2017-05-09 16:06:02 +0100

    Export filters in a seperate file

--- remoteappmanager/static/js/home/controller.js
+++ remoteappmanager/static/js/home/controller.js
@@ -5,29 +5,14 @@ require([
     "home/views/application_view",
     "components/vue/dist/vue",
     "gamodule",
-    'urlutils'
+    "filters"
 ], function(
     models,
     application_list_view,
     application_view,
     Vue,
-    gamodule,
-    urlutils) {
+    gamodule) {
     "use strict";
-
-    Vue.filter('icon_src', function(icon_data) {
-        return (
-            icon_data ?
-            'data:image/png;base64,' + icon_data :
-            urlutils.path_join(
-                window.apidata.base_url, 'static', 'images', 'generic_appicon_128.png'
-            )
-        );
-    });
-
-    Vue.filter('app_name', function(image) {
-        return image.ui_name? image.ui_name: image.name;
-    });
 
     // This model keeps the retrieved content from the REST query locally.
     // It is only synchronized at initial load.
