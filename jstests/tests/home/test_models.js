a79b4223bc9192f244caac3d3529609ec6ca5ecb
martinRenou <martin.renou@isae.fr>
2017-05-10 13:13:56 +0100

    Add tests for the model

--- jstests/tests/home/test_models.js
+++ jstests/tests/home/test_models.js
@@ -2,13 +2,18 @@ define([
     "home/models"
 ], function (models) {
     "use strict";
-    
+
     QUnit.module("home.models");
     QUnit.test("instantiation", function (assert) {
         var model = new models.ApplicationListModel();
+
         assert.equal(model.app_list.length, 0);
+        assert.equal(model.selected_index, null);
+
         model.update().done(function() {
             assert.equal(model.app_list.length, 2);
+            assert.equal(model.selected_index, 0);
+
             assert.equal(model.app_list[0].app_data.image.configurables[0], "resolution");
             assert.equal(model.app_list[0].configurables[0].resolution, "Window");
         });
