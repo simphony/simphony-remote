66a6022118204ef46950e9bf1b0b33139524ab76
martinRenou <martin.renou@isae.fr>
2017-05-12 12:31:07 +0100

    Fixed css of the confirm dialog

--- remoteappmanager/static/js/admin/vue-components/toolkit/ConfirmDialog.js
+++ remoteappmanager/static/js/admin/vue-components/toolkit/ConfirmDialog.js
@@ -17,19 +17,19 @@ define([
         default: function() {}
       }
    },
-    template: '<transition name="modal">' +
-    '  <div class="modal-mask">' +
-    '    <div class="modal-wrapper">' +
-    '      <div class="modal-container">' +
-    '        <div class="modal-header"><slot name="header"><h4>{{ title }}</h4></slot></div>' +
-    '        <div class="modal-body"><slot></slot></div>' +
-    '        <div class="modal-footer text-right">' +
-    '          <button v-if="closeCallback !== undefined" type="button" class="btn btn-default" @click="closeCallback">Cancel</button>' +
-    '          <button class="btn btn-primary primary" @click="okCallback">Ok</button>' +
+    template: '<transition name="modal-fade">' +
+    '    <div class="modal modal-display">' +
+    '      <div class="modal-dialog">' +
+    '        <div class="modal-content">' +
+    '          <div class="modal-header"><slot name="header"><h4>{{ title }}</h4></slot></div>' +
+    '          <div class="modal-body"><slot></slot></div>' +
+    '          <div class="modal-footer text-right">' +
+    '            <button v-if="closeCallback !== undefined" type="button" class="btn btn-default" @click="closeCallback">Cancel</button>' +
+    '            <button class="btn btn-primary primary" @click="okCallback">Ok</button>' +
+    '          </div>' +
     '        </div>' +
     '      </div>' +
     '    </div>' +
-    '  </div>' +
     '</transition>'
   };
 });
