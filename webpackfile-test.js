4ae8f02f2ce4d3c968ad5dcebce455dafa19dae9
martinRenou <martin.renou@isae.fr>
2017-05-17 16:36:58 +0100

    Configure vue-loader so that it uses babel

--- webpackfile-test.js
+++ webpackfile-test.js
@@ -44,10 +44,7 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components)/,
                 use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
+                    loader: 'babel-loader'
                 }
             },
             {
