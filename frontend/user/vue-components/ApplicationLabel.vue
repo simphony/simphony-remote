4bc6e1c96427c20217f70bf8bb665212751f3d07
martinRenou <martin.renou@isae.fr>
2017-05-19 16:20:25 +0100

    ApplicationLable is now a dropdown menu with a close button

--- frontend/user/vue-components/ApplicationLabel.vue
+++ frontend/user/vue-components/ApplicationLabel.vue
@@ -1,62 +1,60 @@
 <template>
-  <div v-if="currentApp !== null" class="container">
-    <img class="app-icon"
-    :src="currentApp.appData.image.icon_128 | iconSrc">
+  <ul v-if="currentApp !== null" class="nav navbar-nav">
+    <li class="dropdown">
+      <a href="#" class="dropdown-toggle cust-padding" data-toggle="dropdown" aria-expanded="false">
+        <img class="app-icon"
+        :src="currentApp.appData.image.icon_128 | iconSrc">
 
-    <button class="stop-button"
-    v-if="currentApp.isRunning()"
-    @click="stopApplication()"
-    :disabled="currentApp.isStopping()">
-      <i class="fa fa-times"></i>
-    </button>
-    <span>{{ currentApp.appData.image | appName }}</span>
-  </div>
+        <span>{{ currentApp.appData.image | appName }}</span>
+      </a>
+
+      <ul class="dropdown-menu" role="menu">
+        <li class="disabled">
+          <a href="#">Application settings</a>
+        </li>
+        <li role="separator" class="divider"></li>
+        <li
+        :class="{ disabled: !currentApp.isRunning() }"
+        @click="stopApplication()">
+          <a href="#">
+            <i class="fa fa-times text-danger"></i>
+            Stop Application
+          </a>
+          <!-- Put other settings here -->
+        </li>
+      </ul>
+    </li>
+  </ul>
 </template>
 
 <script>
-let Vue = require("vuejs");
+  let Vue = require("vuejs");
 
-module.exports = Vue.extend({
-  computed: {
-    currentApp: function() {
-      return this.model.appList[this.model.selectedIndex] || null;
-    }
-  },
+  module.exports = Vue.extend({
+    computed: {
+      currentApp: function() {
+        return this.model.appList[this.model.selectedIndex] || null;
+      }
+    },
 
-  methods: {
-    stopApplication: function(index) {
-      // TODO: Handle error
-      this.model.stopApplication(this.model.selectedIndex);
-    }
-  },
-});
+    methods: {
+      stopApplication: function(index) {
+        // TODO: Handle error
+        this.model.stopApplication(this.model.selectedIndex);
+      }
+    },
+  });
 </script>
 
 <style scoped>
-  .container {
-    margin: 9px;
+  .cust-padding {
+    padding: 9px;
   }
 
   .app-icon {
-    position: relative;
     width: 32px;
     height: 32px;
     border-radius: 20%;
     border: 1px solid #d2d6de;
-    margin-right: 10px;
-  }
-
-  .stop-button {
-    position: absolute;
-    top: 9px;
-    left: 24px;
-    z-index: 100;
-    width: 32px;
-    height: 32px;
-    border: 1px solid rgba(0, 0, 0, 0.8);
-    background-color: rgba(0, 0, 0, 0.7);
-    border-radius: 20%;
-    font-size: 18px;
-    color: rgba(255, 255, 255, 0.7);
   }
 </style>
