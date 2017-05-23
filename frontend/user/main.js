e1ef8279e92a3ace3795227dd6fa199b3c2c6662
martinRenou <martin.renou@isae.fr>
2017-05-23 11:30:48 +0100

    Change user/main.js to introduce the behavior of errorHandler

--- frontend/user/main.js
+++ frontend/user/main.js
@@ -1,4 +1,5 @@
 let gamodule = require("gamodule");
+let ErrorHandler = require("ErrorHandler");
 let ApplicationListModel = require("./ApplicationListModel");
 let ApplicationListView = require("./vue-components/ApplicationListView");
 let ApplicationView = require("./vue-components/ApplicationView");
@@ -8,15 +9,23 @@ require("filters");
 // It is only synchronized at initial load.
 let model = new ApplicationListModel();
 
+// Create errorHandler
+let errorHandler = new ErrorHandler();
+
 // Initialize views
 let appListView = new ApplicationListView({
   el: '#applist',
-  data: function() { return { model: model }; }
+  data: function() { return {
+    model
+  }; }
 });
 
 let appView = new ApplicationView({
   el: '#appview',
-  data: function() { return { model: model }; }
+  data: function() { return {
+    model,
+    errorHandler
+  }; }
 });
 
 // Create GA observer
