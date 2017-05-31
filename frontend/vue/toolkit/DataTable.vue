0cd48143fdfa12c96802f2fc8abbc941028eda28
martinRenou <martin.renou@isae.fr>
2017-05-31 18:09:48 +0000

    We now use admin lte tables

--- frontend/vue/toolkit/DataTable.vue
+++ frontend/vue/toolkit/DataTable.vue
@@ -3,28 +3,30 @@
     <div class="pull-right">
       <button v-for="action in globalActions" class="btn btn-primary" @click="action.callback">{{action.label}}</button>
     </div>
-    <table id="datatable" class="display dataTable">
-      <thead>
-        <tr>
-          <th v-for="header in headers">{{header}}</th>
-          <th v-if="rowActions.length > 0">Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(row, row_index) in rows">
-          <template v-for="(value, col_index) in row">
-            <td v-if="isBoolean(value)"><i class="fa fa-check" v-if="value"></i></td>
-            <td v-else>{{value}}</td>
-          </template>
-          <td>
-            <button v-for="action in rowActions"
-            :class="buttonClassFromType(action.type)"
-            style="margin-right: 10px"
-            @click="action.callback(row)">{{action.label}}</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="table-responsive">
+      <table class="table no-margin">
+        <thead>
+          <tr>
+            <th v-for="header in headers">{{header}}</th>
+            <th v-if="rowActions.length > 0">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(row, row_index) in rows">
+            <template v-for="(value, col_index) in row">
+              <td v-if="isBoolean(value)"><i class="fa fa-check" v-if="value"></i></td>
+              <td v-else>{{value}}</td>
+            </template>
+            <td>
+              <button v-for="action in rowActions"
+              :class="buttonClassFromType(action.type)"
+              style="margin-right: 10px"
+              @click="action.callback(row)">{{action.label}}</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
