6a890e09eca0779ec47e1a933cdb29774d503810
martinRenou <martin.renou@isae.fr>
2017-06-02 11:28:05 +0100

    Fix jstests

--- frontend/tests/tests/vue/components/test_DataTable.js
+++ frontend/tests/tests/vue/components/test_DataTable.js
@@ -6,13 +6,9 @@ QUnit.test("rendering", function (assert) {
   assert.equal(helpers.getRenderedText(DataTable, {
     headers: ["foo", "bar"],
     rows: [[1,2], [3,4]],
-    globalActions: [{
-      label: "New",
-      callback: function() {}
-    }],
     rowActions: [{
       label: "Remove",
       callback: function() {}
     }]
-  }), "New foobar Actions 12 Remove34 Remove");
+  }), "foobar Actions 12 Remove34 Remove");
 });
