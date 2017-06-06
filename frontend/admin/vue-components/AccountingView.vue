7d5355574f7389bee516453fe8a344323ff1cfad
martinRenou <martin.renou@isae.fr>
2017-06-06 16:19:57 +0100

    Fix an other id issue
    When changing the url from an existing id "id1" to an unexisting id "id2", an issue is displayed but the table of the id "id1" is still displayed. In order to fix that we have to empty the table before trying to retrieve informations about the new id, so that if the new id is unknown, the table is empty as expected.

--- frontend/admin/vue-components/AccountingView.vue
+++ frontend/admin/vue-components/AccountingView.vue
@@ -78,9 +78,9 @@
     methods: {
       updateTable: function() {
         this.communicationError = null;
+        this.table.rows = [];
         resources.Accounting.items({filter: JSON.stringify({user_id: this.$route.params.id })})
         .done((identifiers, items) => {
-          this.table.rows = [];
           identifiers.forEach((id) => {
             let item = items[id];
             this.table.rows.push([
