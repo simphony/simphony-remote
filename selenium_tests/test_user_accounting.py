19593f281a732d95d99323b27aee34c8b2869b96
martinRenou <martin.renou@isae.fr>
2017-06-09 15:32:40 +0100

    Add tests

--- selenium_tests/test_user_accounting.py
+++ selenium_tests/test_user_accounting.py
@@ -1,5 +1,6 @@
 from selenium_tests.AdminDriverTest import AdminDriverTest
 from selenium.webdriver.common.by import By
+import os
 
 
 class TestUserAccounting(AdminDriverTest):
@@ -19,6 +20,8 @@ class TestUserAccounting(AdminDriverTest):
         # Click remove button
         self.click_row_action_button("mrenou", "Remove")
 
+        self.wait_until_invisibility_of_row("mrenou")
+
         self.click_modal_footer_button("Ok")
 
     def test_admin_name_header_bug(self):
@@ -27,3 +30,18 @@ class TestUserAccounting(AdminDriverTest):
         self.click_row_action_button("test", "Policies")
 
         self.wait_until_text_inside_element_located(By.CSS_SELECTOR, "span.hidden-xs", "admin")
+
+    def test_user_id(self):
+        self.click_first_element_located(By.LINK_TEXT, "Users")
+
+        self.click_row_action_button("test", "Policies")
+
+        self.wait_until_visibility_of_row("simphonyproject/simphonic-mayavi")
+
+        self.driver.get(os.path.join(
+            self.base_url,
+            "user/admin/#/users/36/accounting"
+        ))
+
+        self.wait_until_presence_of_element_located(By.CSS_SELECTOR, "div.alert-danger")
+        self.wait_until_invisibility_of_row("simphonyproject/simphonic-mayavi")
