189e40bb4c8ba89cd9c9b77cdef4ba8be3828e70
martinRenou <martin.renou@isae.fr>
2017-06-09 11:59:58 +0100

    Add docstring

--- selenium_tests/AdminDriverTest.py
+++ selenium_tests/AdminDriverTest.py
@@ -9,6 +9,24 @@ class AdminDriverTest(RemoteAppDriverTest):
         self.login("admin")
 
     def click_row_action_button(self, row, action_name):
+        """ Click on an action button in the datatable for a specific row
+        Parameters
+        ----------
+        row: String
+            A string which permit to locate the row, it can be:
+            - the index of the row ("2")
+            - a user name ("JohnDoe")
+            - an application name ("File Manager")
+            - a container id ("3514azd65a4z4dazd131")
+        action_name: string
+            The name of the action
+
+        Example
+        -------
+        click_row_action_button("JohnDoe", "Remove")
+        click_row_action_button("JohnDoe", "Policies")
+        click_row_action_button("Mayavi", "Stop")
+        """
         self.click_first_element_located(
             By.XPATH,
             "//tr[td[text()='{}']]/td/button[text()='{}']".format(
