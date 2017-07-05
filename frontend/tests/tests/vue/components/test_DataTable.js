2056e36dd8c1e1d5ecfe219591af0d09c6f0f622
martinRenou <martin.renou@isae.fr>
2017-07-05 11:28:36 +0100

    Add test for columnformatters

--- frontend/tests/tests/vue/components/test_DataTable.js
+++ frontend/tests/tests/vue/components/test_DataTable.js
@@ -1,7 +1,9 @@
-var DataTable = require("toolkit").DataTable;
-var helpers = require("helpers");
+let DataTable = require("toolkit").DataTable;
+let helpers = require("helpers");
+let _ = require("lodash");
 
 QUnit.module("DataTable");
+
 QUnit.test("rendering", function (assert) {
   assert.equal(helpers.getRenderedText(DataTable, {
     headers: ["foo", "bar"],
@@ -15,4 +17,21 @@ QUnit.test("rendering", function (assert) {
       callback: function() {}
     }]
   }), "New foobar Actions 12 Remove34 Remove");
+
+  let checkIconFormatter = function(value) {
+    return '<i class="fa fa-check" v-if="' + value + '"></i>';
+  };
+
+  let truncate = function(value) {
+    return _.truncate(value, {'length': 24});
+  };
+
+  assert.equal(helpers.getRenderedText(DataTable, {
+    headers: ["ID", "name", "checked"],
+    rows: [
+      ["12345678901234567890123456789", "JohnDoe", false],
+      ["98765432109876543210987654321", "JaneDoe", true]
+    ],
+    columnFormatters: [truncate, undefined, checkIconFormatter]
+  }).trim(), "IDnamechecked  123456789012345678901...JohnDoe 987654321098765432109...JaneDoe");
 });
